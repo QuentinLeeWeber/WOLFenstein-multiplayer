fb544a93876961188cd13d31e5653ed040b02271
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2023-12-19 10:48:11 +0100

    Add files via upload

--- client/Game.java
+++ client/Game.java
@@ -1,3 +1,32 @@
-class Game {
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
+class Game extends JPanel{
+	
+	public JPanel(){
+		
+	}
+	
+	
+	private BlenderRender renderer = new BlenderRender();
+	private JFrame frame = new JFrame();
+	
+	public static void main(String[] args){
+		System.out.println("start...");
+		frame.add(this);
+        frame.setSize(1000, 1000);
+        frame.setTitle("WOLFenstein");
+        frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	@Override
+    protected void paintComponent(Graphics g){
+		g.draLine(0, 0, 100, 100);
+		renderer.draw(g);
+	}
 }
