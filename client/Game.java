0c5e33ae94fdd7df86f12dcdab5c3b2b0c9c8961
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2024-01-11 12:56:27 +0100

    - added: Schnittstelle fürs UI

--- client/Game.java
+++ client/Game.java
@@ -13,6 +13,11 @@ class Game extends JPanel{
   private BlenderRender renderer = new BlenderRender();
   private JFrame frame = new JFrame();
   private Level level = new Level1();
+  private UserInterface UI = new UserInterface();
+  private boolean gameRunning = false;
+  
+  public int mouseX;
+  public int mouseY;
   
   public static void main(String[] args){
     game = new Game();
@@ -30,15 +35,26 @@ class Game extends JPanel{
     KeyboardFocusManager m = KeyboardFocusManager.getCurrentKeyboardFocusManager();
     MyKeyEventDispatcher dispatcher = new MyKeyEventDispatcher();
     m.addKeyEventDispatcher(dispatcher);
-  renderer.setLevel(level);
+    MyMouseMotionListener MML = new MyMouseMotionListener();
+    MyMouseListener ML = new MyMouseListener();
+    frame.addMouseMotionListener(MML);
+    frame.addMouseListener(ML);
+    renderer.setLevel(level);
     while(true){
       frame.repaint();  
     }
   }
   
+  //getGame ist dafür da das man von überall aus durch Game.getGame() zuggriff auf die öffentlichen Attribute von Game hat
   public static Game getGame(){
     return game; 
   }
+  
+  //Wird immer dann aufgerufen wenn die linke Maustaste einmal gedrückt wird
+  public void leftClick(){
+    System.out.println(mouseX + "  " +  mouseY);
+    UI.mouseClicked();  
+  } 
   
   // Funktion wird immer dann aufgerufen, wenn gerade eine Taste gedrückt wird, diese wird dann als char übegeben
   public void keyPressed(char c){
@@ -55,12 +71,27 @@ class Game extends JPanel{
     
   }
   
+  //Funktion ist dafür vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
+  public void startGame(){
+    gameRunning = true; 
+  }
+  
+  //Funktion ist dafür vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
+  public void stopGame(){
+     gameRunning = false;
+  }
+  
   //Diese Funktion wird jeden Frame aufgrufen, Graphics g ist der Canvas des Fensters des Spieles
   //Beispielhafte Funktionen von Graphics sind: g.drawLine(x1, y2, x2, y2); // g.drawImage(image, x, y, null);
   @Override
     protected void paintComponent(Graphics g){
-    renderer.draw(g);
-    //System.out.println("repaint");
-    //System.out.println(pressedKey);
+    if(gameRunning){
+      renderer.draw(g); 
+    }
+    UI.update(g, mouseX, mouseY);
+  }
+  
+  public void example(){
+    //System.out.println("example");
   }
 }
