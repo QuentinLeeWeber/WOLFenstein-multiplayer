6da2fa3e0046e33b1d95634b043cc84ebb061cb2
giowa49h8gpr <102609204+giowa49h8gpr@users.noreply.github.com>
2024-01-16 09:56:09 +0100

    UI (#34)
    * Add files via upload
    
    * Add files via upload
    
    ---------
    
    Co-authored-by: Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>

--- client/Game.java
+++ client/Game.java
@@ -4,105 +4,107 @@ import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
-import javax.*;
-import java.util.ArrayList;
+//import javax.*;
 
-class Game extends JPanel {
-    public Game() {
+class Game extends JPanel{
+  public Game(){}
+  
+  private static Game game;
+  private BlenderRender renderer = new BlenderRender();
+  private JFrame frame = new JFrame();
+  private Level level = new Level1();
+  private UserInterface UI = new UserInterface();
+  private boolean gameRunning = false;
+  
+  private Player player = new Player(400, 300);
+  
+  public int mouseX;
+  public int mouseY;
+  
+  public static void main(String[] args){
+    game = new Game();
+    game.start();
+  }
+  
+  // kann eigentlich ignoriert werden
+  private void start(){
+    System.out.println("start...");
+    frame.add(this);
+    frame.setSize(800, 600);
+    frame.setTitle("WOLFenstein");
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    KeyboardFocusManager m = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+    MyKeyEventDispatcher dispatcher = new MyKeyEventDispatcher();
+    m.addKeyEventDispatcher(dispatcher);
+    renderer.level = level;
+    MyMouseMotionListener MML = new MyMouseMotionListener();
+    MyMouseListener ML = new MyMouseListener();
+    frame.addMouseMotionListener(MML);
+    frame.addMouseListener(ML);
+    while(true){
+      frame.repaint();  
     }
-
-    private static Game game;
-    private BlenderRender renderer = new BlenderRender();
-    private JFrame frame = new JFrame();
-    private Level level = new Level1();
-    private UserInterface UI = new UserInterface();
-    private boolean gameRunning = false;
-
-    private Player player = new Player(500, 500);
-
-    public int mouseX;
-    public int mouseY;
-
-    public static void main(String[] args) {
-        game = new Game();
-        game.start();
+  }
+  
+  //getGame ist daf�r da das man von �berall aus durch Game.getGame() zuggriff auf die �ffentlichen Attribute von Game hat
+  public static Game getGame(){
+    return game; 
+  }
+  
+  //Wird immer dann aufgerufen wenn die linke Maustaste einmal gedr�ckt wird
+  public void leftClick(){
+    System.out.println(mouseX + "  " +  mouseY);
+    UI.mouseClicked();  
+  } 
+  
+  // Funktion wird immer dann aufgerufen, wenn gerade eine Taste gedr�ckt wird, diese wird dann als char �begeben
+  public void keyPressed(char c){
+    if (c == 'w') {
+      player.move(10);
+    } else if (c == 'e') {
+      player.turn(90);
+    } else if (c == 'q') {
+      player.turn(-90);
     }
-
-    // kann eigentlich ignoriert werden
-    private void start() {
-        System.out.println("start...");
-        frame.add(this);
-        frame.setSize(1000, 1000);
-        frame.setTitle("WOLFenstein");
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        KeyboardFocusManager m = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-        MyKeyEventDispatcher dispatcher = new MyKeyEventDispatcher();
-        m.addKeyEventDispatcher(dispatcher);
-
-        renderer.level = level;
-        MyMouseMotionListener MML = new MyMouseMotionListener();
-        MyMouseListener ML = new MyMouseListener();
-        frame.addMouseMotionListener(MML);
-        frame.addMouseListener(ML);
-        while (true) {
-            frame.repaint();
-        }
+  }
+  
+  //Funktion wird dann aufgerufen, wenn eine neue Taste gedr�ckt wurde, diese wird dann als char �begeben
+  public void keyTyped(char c){
+    
+  }
+  
+  //Funktion wird dann a aufgerufen wenn eine neue Taste losgelassen wurde, diese wird dann als char �begeben
+  public void keyReleased(char c){
+    
+  }
+  
+  //Funktion ist daf�r vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
+  public void startGame(){
+    gameRunning = true; 
+  }
+  
+  //Funktion ist daf�r vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
+  public void stopGame(){
+     gameRunning = false;
+  }
+  
+  //Diese Funktion wird jeden Frame aufgrufen, Graphics g ist der Canvas des Fensters des Spieles
+  //Beispielhafte Funktionen von Graphics sind: g.drawLine(x1, y2, x2, y2); // g.drawImage(image, x, y, null);
+  @Override
+    protected void paintComponent(Graphics g){
+    if(gameRunning){
+      renderer.draw(g, player); 
     }
+    UI.update(g, mouseX, mouseY);
+  }
+  
+  public void example(){
+    //System.out.println("example");
+  }
 
-    //getGame ist dafür da das man von überall aus durch Game.getGame() zuggriff auf die öffentlichen Attribute von Game hat
-    public static Game getGame() {
-        return game;
-    }
-
-    //Wird immer dann aufgerufen wenn die linke Maustaste einmal gedrückt wird
-    public void leftClick() {
-        System.out.println(mouseX + "  " + mouseY);
-        UI.mouseClicked();
-    }
-
-    // Funktion wird immer dann aufgerufen, wenn gerade eine Taste gedrückt wird, diese wird dann als char übegeben
-    public void keyPressed(char c) {
-        if (c == 'w') {
-            player.move(10);
-        } else if (c == 'e') {
-            player.turn(90);
-        } else if (c == 'q') {
-            player.turn(-90);
-        }
-    }
-
-    //Funktion wird dann aufgerufen, wenn eine neue Taste gedrückt wurde, diese wird dann als char übegeben
-    public void keyTyped(char c) {
-
-    }
-
-    //Funktion wird dann a aufgerufen wenn eine neue Taste losgelassen wurde, diese wird dann als char übegeben
-    public void keyReleased(char c) {
-
-    }
-
-    //Funktion ist dafür vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
-    public void startGame() {
-        gameRunning = true;
-    }
-
-    //Funktion ist dafür vorgesehen, dass das UI einfluss auf das Spiel nehmen kann
-    public void stopGame() {
-        gameRunning = false;
-    }
-
-    //Diese Funktion wird jeden Frame aufgrufen, Graphics g ist der Canvas des Fensters des Spieles
-    //Beispielhafte Funktionen von Graphics sind: g.drawLine(x1, y2, x2, y2); // g.drawImage(image, x, y, null);
-    @Override
-    protected void paintComponent(Graphics g) {
-        if (gameRunning) {
-            renderer.draw(g, player);
-        }
-        UI.update(g, mouseX, mouseY);
-    }
-
-    public void example() {
-        //System.out.println("example");
-    }
+  public boolean getRunning() {
+    return gameRunning;
+  }
 }
--- client/UserInterface.java
+++ client/UserInterface.java
@@ -1,23 +1,36 @@
 import java.awt.*;
 
 class UserInterface {
-
-    private int mouseX;
-    private int mouseY;
-
-    UserInterface() {
-        //game = game.getGame();
+  
+  private int mouseX;
+  private int mouseY;
+  private int losButtonWidth = 150;
+  private int losButtonHeight = 60;
+  private int losButtonX = 400 - losButtonWidth/2;
+  private int losButtonY = 300 - losButtonHeight/2;
+  
+  UserInterface(){
+    //game = game.getGame();
+  }
+  
+  public void update(Graphics g, int _mouseX, int _mouseY){
+    mouseX = _mouseX;
+    mouseY = _mouseY;
+    if(Game.getGame().getRunning() == false) {
+      g.setColor(new Color(255, 0, 0));
+      g.fillRect(losButtonX, losButtonY, losButtonWidth, losButtonHeight);
+      g.setColor(new Color(0, 0, 0));
+      g.drawRect(losButtonX, losButtonY, losButtonWidth, losButtonHeight);
+      g.drawString("LOS", losButtonX + losButtonWidth / 2, losButtonY + losButtonHeight / 2);
     }
-
-    public void update(Graphics g, int _mouseX, int _mouseY) {
-        mouseX = _mouseX;
-        mouseY = _mouseY;
-        g.setColor(new Color(255, 0, 0));
-        g.drawRect(0, 0, 10, 10);
-        Game.getGame().example();
+  }
+  
+  public void mouseClicked(){
+    Game game = Game.getGame();
+    if(mouseX >= losButtonX && mouseX <= losButtonX + losButtonWidth && mouseY >= losButtonY
+            && mouseY <= losButtonY + losButtonHeight && game.getRunning() == false) {
+      System.out.println("Los Button clicked!");
+      game.startGame();
     }
-
-    public void mouseClicked() {
-        Game.getGame().startGame();
-    }
+  }
 }
