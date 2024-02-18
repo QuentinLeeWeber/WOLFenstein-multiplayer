26a2747eb06ceeb45c945250b0620234e97d7237
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2024-02-18 23:39:31 +0100

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,11 @@
+class Enemy extends Kreatur {
+    private int size = 5;
+
+    public Enemy(int x, int y, Level level, String texture) {
+        super(x, y, level, texture);
+        super.boundingBox = new BoundingBox(x, y, size, size);
+    }
+
+    public void update() {
+    }
+}
