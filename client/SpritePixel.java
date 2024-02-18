4ff4375c017123750f21c10c4e12dbcbed09bff7
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2024-02-18 23:44:35 +0100

    Add files via upload

--- client/SpritePixel.java
+++ client/SpritePixel.java
@@ -9,8 +9,8 @@ public class SpritePixel extends Pixel{
     @Override
     public void draw(Graphics g){
 
-        int textureWidth = (int) (float) ((texture.getWidth(null) / distance) * BlenderRender.spriteHeight);
-        int textureHeight = (int) (float) ((texture.getHeight(null) / distance) * BlenderRender.spriteHeight);
+        int textureWidth = (int) (BlenderRender.spriteHeight / distance);
+        int textureHeight = (int) (BlenderRender.spriteHeight / distance);
         
         g.drawImage(texture, x - (int) ((float) textureWidth / 2), 300 - (int) ((float) textureHeight / 2), textureWidth, textureHeight, null);
 
@@ -18,5 +18,7 @@ public class SpritePixel extends Pixel{
             g.setColor(new Color(255, 0, 0));
             g.drawOval(x, 300, 10, 10);
         }
+
+        
     }
 }
