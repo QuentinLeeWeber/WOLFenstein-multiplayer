d54e6ce66f55363c438b0176a274744862ce2d9a
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2024-02-22 12:16:04 +0100

    Revert "Grey horizon patch 1"

--- client/Level2.java
+++
@@ -1,9 +0,0 @@
-class Level2 extends Level {
-  public Level2() {
-    
-    createVector(200, 200, 200, 400);
-    createVector(200, 400, 400, 200);
-    createVector(400, 200, 200, 400);
-    createVector(400, 400, 400, 200);
-  }
-}
