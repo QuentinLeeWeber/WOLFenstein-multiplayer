7438dece062cc36ac1002eba5ee258e129b8890f
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2024-02-26 18:22:06 +0100

    Add files via upload

--- server/EchoThread.java
+++ server/EchoThread.java
@@ -20,9 +20,11 @@ public class EchoThread extends Thread {
             BufferedReader brinp = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String line = brinp.readLine();
             while (line != null) {
-                if (!line.isBlank()) {
-                    System.out.println(line.trim());
-                    Server.notify(line.trim() + "\r\n", id);
+                if (!line.isEmpty()) {
+                    if (line.trim().length() == 0){
+                        System.out.println(line.trim());
+                        Server.notify(line.trim() + "\r\n", id);
+                    }
                 }
                 line = brinp.readLine();
             }
