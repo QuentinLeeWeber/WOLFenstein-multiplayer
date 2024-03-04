26fb5a1311f5136c6ba5f7aeb59274aea27e7443
GreyHorizon <132846260+GreyHorizon@users.noreply.github.com>
2024-03-04 21:42:27 +0100

    Create WallManager.java

---
+++ client/WallManager.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WallManager {
+    
+    // Methode zum Auflisten aller Eckpunkte einer Wand mit anderen Wänden
+    public List<int[]> listAllCornerPoints(List<Wall> walls) {
+        List<int[]> cornerPoints = new ArrayList<>();
+        
+        for (int i = 0; i < walls.size(); i++) {
+            Wall currentWall = walls.get(i);
+            cornerPoints.addAll(currentWall.getCornerPoints());
+            for (int j = i + 1; j < walls.size(); j++) {
+                Wall otherWall = walls.get(j);
+                cornerPoints.addAll(currentWall.getCommonCornerPoints(otherWall));
+            }
+        }
+        
+        return cornerPoints;
+    }
+    
+    // Methode zum Erzeugen von Wänden zwischen allen Eckpunkten
+    public List<Wall> createWallsBetweenCornerPoints(List<int[]> cornerPoints) {
+        List<Wall> newWalls = new ArrayList<>();
+        
+        for (int i = 0; i < cornerPoints.size(); i++) {
+            int[] point1 = cornerPoints.get(i);
+            for (int j = i + 1; j < cornerPoints.size(); j++) {
+                int[] point2 = cornerPoints.get(j);
+                double distance = Math.sqrt(Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2));
+                if (distance > 10) {
+                    newWalls.add(new Wall(point1, point2));
+                }
+            }
+        }
+        
+        return newWalls;
+    }
+    
+    // Methode zum Löschen von Wänden, die einen anderen Eckpunkt auf ihrer Strecke haben
+    public void removeWallsWithInternalCornerPoints(List<Wall> walls, List<int[]> cornerPoints) {
+        walls.removeIf(wall -> {
+            for (int[] point : cornerPoints) {
+                if (wall.hasInternalPoint(point)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+    
+    // Methode zum Löschen von Wänden, die eine Länge von 10 haben
+    public void removeWallsWithLength(List<Wall> walls) {
+        walls.removeIf(wall -> wall.getLength() == 10);
+    }
+}
