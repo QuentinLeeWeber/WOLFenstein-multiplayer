9f305bef77060601b51315ec927ee47f25dcc172
GreyHorizon <132846260+GreyHorizon@users.noreply.github.com>
2024-03-05 10:48:36 +0100

    Update WallManager.java

--- client/WallManager.java
+++ client/WallManager.java
@@ -1,28 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Point;
 
 public class WallManager {
-    
-    // Auflistung aller Eckpunkte einer Wand mit anderen Wänden
-    public List<int[]> listAllCornerPoints(List<Wall> walls) {
-        List<int[]> cornerPoints = new ArrayList<>();
-        
+   
+    // Auflistung aller Eckpunkte einer Wand mit anderen Waenden
+    public List<Integer[]> listAllCornerPoints(List<Wall> walls) {
+        List<Integer[]> cornerPoints = new ArrayList<>();
+       
         for (int i = 0; i < walls.size(); i++) {
             Wall currentWall = walls.get(i);
-            cornerPoints.addAll(currentWall.getCornerPoints());
+          //  cornerPoints.addAll(currentWall.getCornerPoints());
             for (int j = i + 1; j < walls.size(); j++) {
                 Wall otherWall = walls.get(j);
-                cornerPoints.addAll(currentWall.getCommonCornerPoints(otherWall));
+                Point p = Level.calculateWallCorner(currentWall, otherWall);
+                cornerPoints.add(new Integer[]{p.x, p.y});
             }
         }
-        
+       
         return cornerPoints;
     }
-    
-    // Erzeugung von Wänden zwischen allen Eckpunkten
+   
+    // Erzeugung von Waenden zwischen allen Eckpunkten
     public List<Wall> createWallsBetweenCornerPoints(List<int[]> cornerPoints) {
         List<Wall> newWalls = new ArrayList<>();
-        
+       
         for (int i = 0; i < cornerPoints.size(); i++) {
             int[] point1 = cornerPoints.get(i);
             for (int j = i + 1; j < cornerPoints.size(); j++) {
@@ -33,11 +35,11 @@ public class WallManager {
                 }
             }
         }
-        
+       
         return newWalls;
     }
-    
-    // löscht Wände, die einen anderen Eckpunkt auf ihrer Strecke haben
+   
+    // loescht Waende, die einen anderen Eckpunkt auf ihrer Strecke haben
     public void removeWallsWithInternalCornerPoints(List<Wall> walls, List<int[]> cornerPoints) {
         walls.removeIf(wall -> {
             for (int[] point : cornerPoints) {
@@ -48,8 +50,8 @@ public class WallManager {
             return false;
         });
     }
-    
-    // löscht Wände, die eine Länge von 10 haben
+   
+    // loescht Waende, die eine Laenge von 10 haben
     public void removeWallsWithLength(List<Wall> walls) {
         walls.removeIf(wall -> wall.getLength() == 10);
     }
