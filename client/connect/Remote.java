b26e28795d62a04224011e928a9a3b84093a0c82
Quentin Weber <77012644+QuentinLeeWeber@users.noreply.github.com>
2025-02-05 15:20:41 +0100

    Update Remote.java

--- client/connect/Remote.java
+++ client/connect/Remote.java
@@ -23,7 +23,7 @@ public class Remote {
         try{
             clientSocket = new Socket(ip, 6969);
         } catch(Exception e){
-            clientSocket = new Socket("quentman.hopto.org", 6969);
+            clientSocket = new Socket("quentmann.mywire.org", 6969);
             System.out.println("using default ip : quentman.hopto.org, because server is unreachable");
         }
         out = new PrintWriter(clientSocket.getOutputStream(), true);
